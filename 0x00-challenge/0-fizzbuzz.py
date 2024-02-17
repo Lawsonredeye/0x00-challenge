024a1c11ab6e98dab99e8e60d0009bf60d3d7200
lawsonredeye <omoregbeeolawson@gmail.com>
2024-02-17 22:11:06 +0100

    modified fizzbuzz

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -21,7 +21,7 @@ def fizzbuzz(n):
         if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
         if (i % 3) == 0:
-            tmp_result.append("FizzBuzz")
+            tmp_result.append("Fizz")
         elif (i % 5) == 0:
             tmp_result.append("Buzz")
         else:
